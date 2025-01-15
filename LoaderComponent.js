7688d94f198d0f980cb13f29bc0441447e5619d7
krishnachaitanya4 <krishnachaitanya055@gmail.com>
2025-01-15 20:51:45 +0530

    pre final commit 00

--- LoaderComponent.js
+++ LoaderComponent.js
@@ -17,11 +17,11 @@ class LoaderComponent {
             </div>
             <style>
                 #loader {
-                    position: fixed;
+                    position: absolute;
                     top: 0;
                     left: 0;
-                    width: 100%;
-                    height: 100%;
+                    width: 100vw;
+                    height: 100vh;
                     background-color: #000000;
                     z-index: 9999;
                     display: flex;
